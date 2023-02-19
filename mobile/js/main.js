688dc59fb6d45c5d8d185a746bbf880047337510
ananass <sitthichai.wa@gmail.com>
2023-02-19 22:42:26 +0700

    Remove unused

--- mobile/js/main.js
+++ mobile/js/main.js
@@ -260,20 +260,6 @@ function takeSnapshot() {
   return base64
 }
 
-function mirrorImage(ctx, image, x = 0, y = 0, horizontal = false, vertical = false) {
-  ctx.save() // save the current canvas state
-  ctx.setTransform(
-    horizontal ? -1 : 1,
-    0, // set the direction of x axis
-    0,
-    vertical ? -1 : 1, // set the direction of y axis
-    x + (horizontal ? image.width : 0), // set the x origin
-    y + (vertical ? image.height : 0) // set the y origin
-  )
-  ctx.drawImage(image, 0, 0)
-  ctx.restore() // restore the state as it was when this function was called
-}
-
 // https://hackernoon.com/how-to-use-javascript-closures-with-confidence-85cd1f841a6b
 // closure; store this in a variable and call the variable as function
 // eg. var takeSnapshotUI = createClickFeedbackUI();
